29e74e1b147356376e69043038ef19ef6d50f1d0
Conor Kirby <conorkirby1@gmail.com>
2023-08-25 23:50:53 +0100

    Add project files.

---
+++ BlazorGameWorld/Program.cs
@@ -0,0 +1,42 @@
+using BlazorGameWorld.Data;
+using BlazorSignalRApp.Server.Hubs;
+using blazorWords.Data;
+using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.Web;
+using Microsoft.AspNetCore.ResponseCompression;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddSignalR();
+builder.Services.AddRazorPages();
+builder.Services.AddServerSideBlazor();
+builder.Services.AddResponseCompression(opts =>
+{
+	opts.MimeTypes = ResponseCompressionDefaults.MimeTypes.Concat(
+		new[] { "application/octet-stream" });
+});
+builder.Services.AddTransient<IWordService, WordService>();
+builder.Services.AddSingleton<WeatherForecastService>();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+	app.UseExceptionHandler("/Error");
+	// The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+	app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.MapBlazorHub();
+app.MapHub<WordHub>("/wordhub");
+app.MapFallbackToPage("/_Host");
+
+app.Run();
