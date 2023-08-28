0e3c823e993614096cdc5704077335dd41363033
Conor Kirby <conorkirby1@gmail.com>
2023-08-28 18:19:51 +0100

    Added css and images, working on mongoDB connection

--- BlazorGameWorld/Models/Words.cs
+++ BlazorGameWorld/Models/Words.cs
@@ -3,11 +3,11 @@ using MongoDB.Bson;
 using MongoDB.Bson.Serialization.Attributes;
 
 public class Words{
-     [BsonId]
-        [BsonRepresentation(BsonType.ObjectId)]
-        public string Id { get; set; }
+    [BsonId]
+    [BsonRepresentation(BsonType.ObjectId)]
+    public string Id { get; set; }
 
-        [BsonElement("word")]
-        [Required]
-        public string word { get; set; }
+    [BsonElement("word")]
+    [Required]
+    public string word { get; set; }
 }
